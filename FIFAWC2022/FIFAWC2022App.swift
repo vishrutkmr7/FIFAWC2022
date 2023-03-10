df26ba3d823969b973e4796d27f299626996eb9d
Vishrut Jha <vishrutkmr7@icloud.com>
2023-03-10 16:56:41 -0700

    Initial Commit

---
+++ FIFAWC2022/FIFAWC2022App.swift
@@ -0,0 +1,17 @@
+//
+//  FIFAWC2022App.swift
+//  FIFAWC2022
+//
+//  Created by Vishrut Jha on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FIFAWC2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FIFAWC2022UITests/FIFAWC2022UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FIFAWC2022UITestsLaunchTests.swift
+//  FIFAWC2022UITests
+//
+//  Created by Vishrut Jha on 3/10/23.
+//
+
+import XCTest
+
+final class FIFAWC2022UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
